a8c567b4bd475fbc8979c5f9582ec81756988f63
Jeff Trespalacios <jeff.trespalacios@gmail.com>
2014-01-17 10:33:39 -0500

    add assertion for testing login user initialization

--- objc gitlab apiTests/models/GLUserTests.m
+++ objc gitlab apiTests/models/GLUserTests.m
@@ -41,43 +41,12 @@ static NSString *const kUserLoginResponseString = @"{ \"id\": 1, \"username\": \
     knownUser.twitter = @"jsmithy";
     knownUser.themeId = 1;
     
+    NSData *loginData = [kUserLoginResponseString dataUsingEncoding:NSUTF8StringEncoding];
+    NSDictionary *loginJson = [NSJSONSerialization JSONObjectWithData:loginData options:0 error:nil];
+    GLUser *testUser = [[GLUser alloc] initWithJSON:loginJson];
     
     
-    
-    
+    XCTAssertEqualObjects(knownUser, testUser, @"User generated from login data not initialized correctly");
 }
 
 @end
-
-/*
- // id
- @property (nonatomic, assign) int64_t userId;
- // username
- @property (nonatomic, copy) NSString *username;
- // email
- @property (nonatomic, copy) NSString *email;
- // name
- @property (nonatomic, copy) NSString *name;
- // skype
- @property (nonatomic, copy) NSString *skype;
- // linkedin
- @property (nonatomic, copy) NSString *linkedin;
- // twitter
- @property (nonatomic, copy) NSString *twitter;
- // provider
- @property (nonatomic, copy) NSString *provider;
- // state
- @property (nonatomic, copy) NSString *state;
- // created_at
- @property (nonatomic, strong) NSDate *createdAt;
- // bio
- @property (nonatomic, copy) NSString *bio;
- // extern_uid
- @property (nonatomic, copy) NSString *externUid;
- // theme_id
- @property (nonatomic, assign) int32_t themeId;
- // color_scheme_id
- @property (nonatomic, assign) int32_t colorSchemeId;
- // is_admin
- @property (nonatomic, getter = isAdmin) BOOL admin;
-*/
