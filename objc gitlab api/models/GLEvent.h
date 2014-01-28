b308bbafd633174599669487777df232c335098e
Jon Staff <jstaff@indatus.com>
2014-01-28 12:52:11 -0500

    Added a new model for Event.

---
+++ objc gitlab api/models/GLEvent.h
@@ -0,0 +1,39 @@
+//
+//  GLEvent.h
+//  objc gitlab api
+//
+//  Created by Jon Staff on 1/28/14.
+//  Copyright (c) 2014 Indatus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "GLJsonProtocol.h"
+
+@interface GLEvent : NSObject <GLJsonProtocol>
+
+// title
+@property (nonatomic, copy) NSString *title;
+// project_id
+@property (nonatomic, assign) int64_t projectId;
+// action_name
+@property (nonatomic, copy) NSString *actionName;
+// target_id
+@property (nonatomic, assign) int64_t targetId;
+// target_type
+@property (nonatomic, copy) NSString *targetType;
+// author_id
+@property (nonatomic, assign) int64_t authorId;
+// data
+@property (nonatomic, strong) NSDictionary *data;
+// target_title
+@property (nonatomic, copy) NSString *targetTitle;
+
+- (BOOL)isEqual:(id)other;
+
+- (BOOL)isEqualToEvent:(GLEvent *)event;
+
+- (NSUInteger)hash;
+
+- (NSString *)description;
+
+@end
---
+++ objc gitlab api/models/GLEvent.m
@@ -0,0 +1,116 @@
+//
+//  GLEvent.m
+//  objc gitlab api
+//
+//  Created by Jon Staff on 1/28/14.
+//  Copyright (c) 2014 Indatus. All rights reserved.
+//
+
+#import "GLEvent.h"
+
+static NSString * const kKeyTitle = @"title";
+static NSString * const kKeyProjectId = @"project_id";
+static NSString * const kKeyActionName = @"action_name";
+static NSString * const kKeyTargetId = @"target_id";
+static NSString * const kKeyTargetType = @"target_type";
+static NSString * const kKeyAuthorId = @"author_id";
+static NSString * const kKeyData = @"data";
+static NSString * const kKeyTargetTitle = @"target_title";
+
+@implementation GLEvent
+
+- (instancetype)initWithJSON:(NSDictionary *)json
+{
+    if (self = [super init]) {
+        _title = json[kKeyTitle];
+        _projectId = [json[kKeyProjectId] longLongValue];
+        _actionName = json[kKeyActionName];
+        _targetId = [json[kKeyTargetId] longLongValue];
+        _targetType = json[kKeyTargetType];
+        _authorId = [json[kKeyAuthorId] longLongValue];
+        _data = [json[kKeyData] dictionaryRepresentation];
+        _targetTitle = json[kKeyTargetTitle];
+    }
+    
+    return self;
+}
+
+- (NSDictionary *)jsonRepresentation
+{
+    id null = (id)[NSNull null];
+    return @{
+             kKeyTitle: _title ?: null,
+             kKeyActionName: _actionName ?: null,
+             kKeyTargetId: @(_targetId),
+             kKeyTargetType: _targetType ?: null,
+             kKeyAuthorId: @(_authorId),
+             kKeyData: _data ?: null,
+             kKeyTargetTitle: _targetTitle ?: null
+             };
+}
+
+- (BOOL)isEqual:(id)other
+{
+    if (other == self)
+        return YES;
+    if (!other || ![[other class] isEqual:[self class]])
+        return NO;
+
+    return [self isEqualToEvent:other];
+}
+
+- (BOOL)isEqualToEvent:(GLEvent *)event
+{
+    if (self == event)
+        return YES;
+    if (event == nil)
+        return NO;
+    if (self.title != event.title && ![self.title isEqualToString:event.title])
+        return NO;
+    if (self.projectId != event.projectId)
+        return NO;
+    if (self.actionName != event.actionName && ![self.actionName isEqualToString:event.actionName])
+        return NO;
+    if (self.targetId != event.targetId)
+        return NO;
+    if (self.targetType != event.targetType && ![self.targetType isEqualToString:event.targetType])
+        return NO;
+    if (self.authorId != event.authorId)
+        return NO;
+    if (self.data != event.data && ![self.data isEqualToDictionary:event.data])
+        return NO;
+    if (self.targetTitle != event.targetTitle && ![self.targetTitle isEqualToString:event.targetTitle])
+        return NO;
+    return YES;
+}
+
+- (NSUInteger)hash
+{
+    NSUInteger hash = [self.title hash];
+    hash = hash * 31u + (NSUInteger) self.projectId;
+    hash = hash * 31u + [self.actionName hash];
+    hash = hash * 31u + (NSUInteger) self.targetId;
+    hash = hash * 31u + [self.targetType hash];
+    hash = hash * 31u + (NSUInteger) self.authorId;
+    hash = hash * 31u + [self.data hash];
+    hash = hash * 31u + [self.targetTitle hash];
+    return hash;
+}
+
+- (NSString *)description
+{
+    NSMutableString *description = [NSMutableString stringWithFormat:@"<%@: ", NSStringFromClass([self class])];
+    [description appendFormat:@"self.title=%@", self.title];
+    [description appendFormat:@", self.projectId=%qi", self.projectId];
+    [description appendFormat:@", self.actionName=%@", self.actionName];
+    [description appendFormat:@", self.targetId=%qi", self.targetId];
+    [description appendFormat:@", self.targetType=%@", self.targetType];
+    [description appendFormat:@", self.authorId=%qi", self.authorId];
+    [description appendFormat:@", self.data=%@", self.data];
+    [description appendFormat:@", self.targetTitle=%@", self.targetTitle];
+    [description appendString:@">"];
+    return description;
+}
+
+
+@end
