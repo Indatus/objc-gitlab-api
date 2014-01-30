1fe6ab57d393ef5e4f69eb3c64cf0d705277b2ec
Jeff Trespalacios <jeff.trespalacios@gmail.com>
2014-01-30 15:16:11 -0500

    add snippet test

---
+++ objc gitlab apiTests/models/GLSnippetTests.m
@@ -0,0 +1,57 @@
+//
+//  GLSnippetTests.m
+//  objc gitlab api
+//
+//  Created by Jeff Trespalacios on 1/30/14.
+//  Copyright (c) 2014 Indatus. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "GLTestUtility.h"
+#import "GLGitlabApi.h"
+#import "GLSnippet.h"
+#import "GLUser.h"
+
+@interface GLSnippetTests : XCTestCase
+
+@end
+
+@implementation GLSnippetTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testSnippetJsonInit
+{
+    NSDateFormatter *formatter = [[GLGitlabApi sharedInstance] gitLabDateFormatter];
+    NSDictionary *snippetJson = [GLTestUtility loadJsonFile:@"snippet"];
+    
+    GLSnippet *testSnippet = [[GLSnippet alloc] initWithJSON:snippetJson];
+    GLSnippet *knownSnippet = [GLSnippet new];
+    knownSnippet.snippetId = 1;
+    knownSnippet.title = @"test";
+    knownSnippet.file_name = @"add.rb";
+    knownSnippet.author = [GLUser new];
+    knownSnippet.author.userId = 1;
+    knownSnippet.author.username = @"john_smith";
+    knownSnippet.author.email = @"john@example.com";
+    knownSnippet.author.name = @"John Smith";
+    knownSnippet.author.state = @"active";
+    knownSnippet.author.createdAt = [formatter dateFromString:@"2012-05-23T08:00:58Z"];
+    knownSnippet.updatedAt = [formatter dateFromString:@"2012-06-28T10:52:04Z"];
+    knownSnippet.createdAt = [formatter dateFromString:@"2012-06-28T10:52:04Z"];
+    
+    XCTAssertEqualObjects(knownSnippet, testSnippet, @"Snippet initialied from JSON incorrectly");
+    
+}
+
+@end
